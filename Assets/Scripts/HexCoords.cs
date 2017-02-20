9a39b5fb14afcdf9b258017b957ac4d227b511c2
Jiri Korinek <Jiri Korinek>
2017-02-20 20:26:10 +0100

    some hex commit xd

---
+++ Assets/Scripts/HexCoords.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class HexCoords
+{
+    public static readonly float HEIGHT = 0.728f;
+
+    public static void Axial2Cube()
+    {
+        
+    }
+
+    public static void Cube2Axial()
+    {
+        
+    }
+
+    public static void Axial2World()
+    {
+        
+    }
+
+    public static void World2Axial()
+    {
+        
+    }
+
+    public static void Cube2World()
+    {
+        
+    }
+
+    public static void World2Cube()
+    {
+        
+    }
+
+    public static Vector3 Offset2World(int x, int y, int z)
+    {
+        float width = (Mathf.Sqrt(3f)/2f)*HEIGHT;
+        float xCoord = x*width + (z%2 == 0 ? width/2f : width);
+        float zCoord = z*HEIGHT;
+        return new Vector3(xCoord, y, zCoord);
+    }
+
+
+}
+
---
+++ Assets/Scripts/HexTester.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexTester : MonoBehaviour
+{
+
+    public Transform DebugCube;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Debug.Log(HexCoords.Offset2World(0, 0, 0));
+        Debug.Log(HexCoords.Offset2World(1, 0, 0));
+        Debug.Log(HexCoords.Offset2World(2, 0, 0));
+
+        Debug.Log(HexCoords.Offset2World(0, 0, 1));
+        Debug.Log(HexCoords.Offset2World(1, 0, 1));
+        Debug.Log(HexCoords.Offset2World(2, 0, 1));
+
+	    for (int i = 0; i < 7; i++)
+	    {
+	        for (int j = 0; j < 7; j++)
+	        {
+	            Transform hex = Instantiate(DebugCube);
+	            hex.position = HexCoords.Offset2World(i, 0, j);
+	        }
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

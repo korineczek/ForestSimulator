036227fb9c518d41c0e2289719e04e759075b688
Jiri Korinek <kormaister@gmail.com>
2017-02-21 16:24:38 +0100

    added numbers to the project

--- Assets/Scripts/HexTester.cs
+++ Assets/Scripts/HexTester.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HexTester : MonoBehaviour
 {
 
     public Transform DebugCube;
     private Transform[,] hexesTransforms;
+    private Tile[,] tileArray;
 
 	// Use this for initialization
 	void Start ()
 	{
         hexesTransforms = new Transform[10,10];
+        tileArray = new Tile[10,10];
 
 	    for (int i = 0; i < 10; i++)
 	    {
@@ -20,11 +23,16 @@ public class HexTester : MonoBehaviour
 	            Transform hex = Instantiate(DebugCube);
 	            hex.position = HexCoords.Offset2World(i, 0, j);
 	            hexesTransforms[i,j] = hex;
+                tileArray[i,j] = new Tile(i,j, 0);
+
+                //set tile debug text
+	            hexesTransforms[i, j].GetChild(0).GetChild(0).GetComponent<Text>().text = i + " " + j +"\n" + tileArray[i,j].CubeCoordinates.ToString();
+
 	        }
 	    }
 
 
-	    List<Vector3> testRange = HexCoords.HexRange(HexCoords.Offset2Cube(4,4), 2);
+	    List<Vector3> testRange = HexCoords.HexRange(HexCoords.Offset2Cube(4,4), 3);
 	    foreach (Vector3 hex in testRange)
 	    {
 	        Debug.Log(hex);
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The class used for storing information about individual tiles
+/// </summary>
+public class Tile
+{
+
+    public Vector2 OffsetCoordinates;
+    public Vector3 CubeCoordinates;
+    public Vector3 WorldCoordinates;
+
+    public Tile(int col, int row, int height)
+    {
+        OffsetCoordinates = new Vector2(col,row);
+        CubeCoordinates = HexCoords.Offset2Cube(col, row);
+        WorldCoordinates = HexCoords.Offset2World(col, height, row);
+    }
+
+    public Tile()
+    {
+        
+    }
+
+}

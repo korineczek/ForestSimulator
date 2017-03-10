b6f2d12f943a6e6ba139d110ea00b0b0acf09eb6
DESKTOP-FHRSA1R\Sandra Fagerli <sandra@fagerli.in>
2017-03-10 09:14:13 +0100

    prefabapp

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
+    [Header("Zoom Settings")]
     public float maxZoom = 10f;
     public float minZoom = 3f;
-
-    public float speed = 2.0f;
+    public float scrollSpeed;
     private float zoomS = 2.0f;
-    // Use this for initialization
 
-    public float minix = -20.0f;
-    public float maxx = 60.0f;
-    public float minY = -45.0f;
-    public float maxY = 45.0f;
+    [Header("Movement Settings"), Range(1f,10f)]
+    public float movementSpeed = 2.0f;
+    [Range(1f, 10f)]
+
+    [Header("Rotation Settings")]
+    Quaternion defaultRotation;
+    public float leftLimit = -20f;
+    public float rightLimit = 20f;
+    public float downLimit = -45.0f;
+    public float upLimit = -10f;
     public float sensX = 100.0f;
     public float sensY = 100.0f;
 
@@ -22,26 +27,40 @@ public class CameraScript : MonoBehaviour {
     float rotationX = 0.0f;
          
 	void Start () {
-		
+        defaultRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float moveX = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
 
-        Vector3 newposition = new Vector3(moveX, 0, moveZ);
-        transform.position += newposition;
-      
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if(transform.position.y + scroll * -zoomS > minZoom && transform.position.y + scroll * -zoomS < maxZoom) {
-            transform.Translate(0, scroll * -zoomS, scroll * zoomS, Space.World);
+        Vector3 flatForward = Vector3.Scale(transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        transform.position += transform.right * moveX;
+        transform.position += flatForward * moveZ;
+
+        //Vector3 newposition = new Vector3(moveX, 0, moveZ);
+        //transform.position += newposition;
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+
+        Vector3 zoomVector = transform.forward * scroll;
+        Vector3 whereWeTryToGo = (transform.position + zoomVector);
+        if (whereWeTryToGo.y > minZoom && whereWeTryToGo.y < maxZoom)
+        {
+            transform.position += zoomVector;
         }
+
+        //if (transform.position.y + scroll * -zoomS > minZoom && transform.position.y + scroll * -zoomS < maxZoom) {
+        //    transform.Translate(0, scroll * -zoomS, scroll * zoomS, Space.World);
+        //}
         if (Input.GetMouseButton(2))
         {
             rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+            rotationX = Mathf.Clamp(rotationX, leftLimit, rightLimit);
             rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
-            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            rotationY = Mathf.Clamp(rotationY, downLimit, upLimit);
             transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
         }
     }

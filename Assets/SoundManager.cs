4df7d9055b4fd2dfa93418d7fc78f2266fe0392e
DESKTOP-FHRSA1R\Sandra Fagerli <sandra@fagerli.in>
2017-04-17 14:40:28 +0200

    umh sound

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    public AudioSource MusicPlayer;
+    public AudioSource WeatherSounds;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

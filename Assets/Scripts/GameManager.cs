1021ac448cca65e07114d27c7e883cd50ba0e033
Jiri Korinek <kormaister@gmail.com>
2017-04-24 13:04:46 +0200

    fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(HexGrid))]
-[RequireComponent(typeof(Overlay))]
 public class GameManager : MonoBehaviour
 {
 
@@ -181,7 +180,7 @@ public class GameManager : MonoBehaviour
             }
         }
         //End of update
-        UI.UpdateCanvas();
+        //UI.UpdateCanvas();
     }
 
     private void KillTrees()
